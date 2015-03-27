84a63d2b822b4e5412af0e192b4ccbdcc5aeb296
Jim Kang <jimkang@gmail.com>
2015-03-27 00:11:15 -0400

    Made render() reentrant.

--- renderer.js
+++ renderer.js
@@ -1,33 +1,47 @@
 var d3 = require('./lib/d3-small');
 
+var levelNameRoot;
+var levelNumberRoot;
+
 function render(classProfile) {
   var root = d3.select('#root');
 
-  var levelNameRoot = root.append('div')
-    .attr('id', 'level-name-root')
-    .classed('name-column', true);
+  if (!levelNameRoot) {
+    levelNameRoot = root.append('div')
+      .attr('id', 'level-name-root')
+      .classed('name-column', true);
 
-  appendColumnHeader(levelNameRoot, 'Level Title');
+    appendColumnHeader(levelNameRoot, 'Level Title');
+  }
+
 
   var levelNames = levelNameRoot.selectAll('.level-name')
     .data(classProfile.levelNames);
   levelNames.enter().append('div').classed('level-name', true);
+
+  fadeAndRemove(levelNames.exit());
+
   levelNames.text(identity);
 
-  var levelNumberRoot = root.append('div')
-    .attr('id', 'level-number-root')
-    .classed('level-number-column', true)
-    .classed('centered-text', true);
+  if (!levelNumberRoot) {
+    levelNumberRoot = root.append('div')
+      .attr('id', 'level-number-root')
+      .classed('level-number-column', true)
+      .classed('centered-text', true);
 
-  appendColumnHeader(
-    levelNumberRoot, 
-    classProfile.hitDie + '-Sided Dice for Accumulated Hit Points'
-  );
+    appendColumnHeader(
+      levelNumberRoot, 
+      classProfile.hitDie + '-Sided Dice for Accumulated Hit Points'
+    );
+  }
 
   var levelNumberData = d3.range(1, classProfile.levelNames.length + 1);
   var levelNumbers = levelNumberRoot.selectAll('.level-number')
     .data(levelNumberData);
   levelNumbers.enter().append('div').classed('level-number', true);
+
+  fadeAndRemove(levelNumbers.exit());
+
   levelNumbers.text(identity);
 }
 
@@ -40,6 +54,14 @@ function appendColumnHeader(columnRoot, text, innerClass) {
 
 function identity(d) {
   return d;
+}
+
+function fadeAndRemove(selection) {
+  selection
+    .classed('fade-out', true)
+    .transition().delay(500)
+    .remove();
+
 }
 
 module.exports = {
