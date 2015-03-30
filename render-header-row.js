7a803fd7295040a2078fec11da7e93b7268cde67
Jim Kang <jimkang@gmail.com>
2015-03-30 16:42:39 -0400

    Tons of refactoring, mostly to support a row-based layout so that long level titles don't cause a misalignment with level numbers when the width of the view is small.

---
+++ render-header-row.js
@@ -0,0 +1,57 @@
+var d3 = require('./lib/d3-small');
+
+var headerRow;
+
+function renderHeaderRow(root, classProfile) {
+  if (!headerRow) {
+    headerRow = root.append('tr').attr('id', 'header-row').classed('row', true);
+  }
+
+  var headerSpecs = [
+    {
+      columnClass: 'name-column',
+      text: 'Level Title',
+      shortText: 'Title'
+    },
+    {
+      columnClass: 'hit-dice-column',
+      text: classProfile.hitDie + '-Sided Dice for Accumulated Hit Points',
+      shortText: 'Hit Dice (' + classProfile.hitDie + ')'
+    },
+    {
+      columnClass: 'level-number-column',
+      text: 'Experience Level',
+      shortText: 'Level'
+    },
+  ];
+
+  headerSpecs.map(addRowOpt).forEach(renderColumnHeader);
+
+  function addRowOpt(opts) {
+    opts.row = headerRow;
+    return opts;
+  }  
+}
+
+function renderColumnHeader(opts) {
+  var header = opts.row.select('.' + opts.columnClass);
+  if (header.empty()) {
+    header = opts.row
+      .append('td').classed('column-header', true)
+      .classed(opts.columnClass, true)
+      // .append('div').classed('inner-header', true);
+    header
+      .append('span')
+      .classed('full-header-text', true)
+    header
+      .append('span')
+      .classed('short-header-text', true)
+  }
+
+  header.select('.full-header-text').text(opts.text);
+  header.select('.short-header-text').text(opts.shortText);
+
+  return header;
+}
+
+module.exports = renderHeaderRow;
--- renderer.js
+++ renderer.js
@@ -1,7 +1,9 @@
 var d3 = require('./lib/d3-small');
+var profileToRows = require('./profile-to-rows');
+var accessor = require('accessor');
+var renderHeaderRow = require('./render-header-row');
 
 var root;
-var columnRoots = {};
 
 function render(classProfile) {
   d3.select('#title').text(classProfile.className + ' TABLE');
@@ -10,89 +12,32 @@ function render(classProfile) {
     root = d3.select('#root');
   }
 
-  renderColumnData({
-    rootName: 'levelNameRoot',
-    columnId: 'level-name-root',
-    columnClass: 'name-column',
-    headerText: 'Level Title',
-    dataClass: 'level-name',
-    data: classProfile.levelNames,
-    centerText: false
-  });
+  var rows = profileToRows(classProfile);
 
-  renderColumnData({
-    rootName: 'hitDiceRoot',
-    columnId: 'hit-dice-root',
-    columnClass: 'hit-dice-column',
-    headerText: classProfile.hitDie + '-Sided Dice for Accumulated Hit Points',
-    dataClass: 'hit-dice',
-    data: d3.range(1, classProfile.levelNames.length + 1)
-  });
+  renderHeaderRow(root, classProfile);
 
-  renderColumnData({
-    rootName: 'levelNumberRoot',
-    columnId: 'level-number-root',
-    columnClass: 'level-number-column',
-    headerText: 'Experience Level',
-    dataClass: 'level-number',
-    data: d3.range(1, classProfile.levelNames.length + 1)
-  });
+  var rowElements = root.selectAll('.row:not(#header-row)')
+    .data(rows, accessor('levelNumber'));
 
+  rowElements.enter().append('tr').classed('row', true).each(addRowCells)
+  rowElements.each(updateRowCells);
+  fadeAndRemove(rowElements.exit());
 }
 
-function renderColumnData(opts) {
-  var rootName;
-  var columnId;
-  var columnClass;
-  var headerText;
-  var dataClass;
-  var data;
-  var centerText = true;
+function addRowCells() {
+  var row = d3.select(this);
 
-  if (opts) {
-    rootName = opts.rootName;
-    columnId = opts.columnId;
-    columnClass = opts.columnClass;
-    headerText = opts.headerText;
-    dataClass = opts.dataClass;
-    data = opts.data;
-    if ('centerText' in opts) {
-      centerText = opts.centerText;
-    }
-  }
-
-  var columnRoot = columnRoots[rootName];
-
-  if (!columnRoot) {
-    columnRoot = root.append('div').attr('id', columnId)
-      .classed(columnClass, true);
-
-    if (centerText) {
-      columnRoot.classed('centered-text', true);
-    }
-
-    appendColumnHeader(columnRoot, headerText);
-    columnRoots[rootName] = columnRoot;
-  }
-
-  columnRoot.select('.inner-header').text(headerText);
-
-  var dataElements = columnRoot.selectAll('.' + dataClass).data(data);
-  dataElements.enter().append('div').classed(dataClass, true);
-
-  fadeAndRemove(dataElements.exit());
-
-  dataElements.text(identity);
+  row.append('td').classed('level-number-column', true);
+  row.append('td').classed('hit-dice-column', true);
+  row.append('td').classed('name-column', true);
 }
 
-function appendColumnHeader(columnRoot) {
-  columnRoot
-    .append('div').classed('column-header', true)
-    .append('div').classed('inner-header', true);
-}
+function updateRowCells(d) {
+  var row = d3.select(this);
 
-function identity(d) {
-  return d;
+  row.select('.level-number-column').text(d.levelNumber);
+  row.select('.hit-dice-column').text(d.hd);
+  row.select('.name-column').text(d.name);
 }
 
 function fadeAndRemove(selection) {
