bbbde0650080442c70ab060e843bdc4a2bc144b6
Jim Kang <jimkang@gmail.com>
2015-04-02 22:35:08 -0400

    Fixed router not using the defaultResponder.

--- router.js
+++ router.js
@@ -1,25 +1,23 @@
 function createRouter(opts) {
   var routes;
-  var defaultResponder;
 
   if (opts) {
     routes = opts.routes;
-    defaultResponder = opts.defaultResponder;
   }
 
   function route(e) {
-    // if (e.state) {
-    // }
-
     var hashParts = location.hash.slice(2).split('/');
     if (hashParts.length > 1) {
       var responder = routes[hashParts[0]];
       if (responder) {
         responder(hashParts[1]);
       }
-      else if (defaultResponder) {
-        defaultResponder(hashParts[1]);
+      else if (routes.defaultResponder) {
+        routes.defaultResponder(hashParts[1]);
       }
+    }
+    else if (routes.defaultResponder) {
+      routes.defaultResponder();
     }
   }
 
